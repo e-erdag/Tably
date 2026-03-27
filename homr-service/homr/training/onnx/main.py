dc7e62e28a8290040c23e2e61512656af6bac8c1
Ege <ee.erdag@gmail.com>
2026-03-27 14:15:23 -0400

    Add HOMR files and update README

---
+++ homr-service/homr/training/onnx/main.py
@@ -0,0 +1,58 @@
+# ruff: noqa: T201
+
+import os
+
+from homr.segmentation.config import segnet_path_onnx_fp16
+from homr.transformer.configs import Config
+from training.onnx.convert import (
+    convert_decoder,
+    convert_encoder,
+    convert_segnet,
+)
+from training.onnx.quantization import quantization_fp16, quantization_int8
+from training.onnx.simplify import main as simplify_onnx_model
+from training.onnx.split_weights import split_weights
+
+
+def convert_segnet_quant() -> None:
+    """
+    Converts and Quantizes the Segnet.
+    """
+    path_to_segnet = convert_segnet()
+    simplify_onnx_model(path_to_segnet)
+    print(path_to_segnet)
+    path_to_segnet_fp16 = quantization_fp16(path_to_segnet, segnet_path_onnx_fp16)
+    print(path_to_segnet_fp16)
+
+
+def convert_transformer_quant() -> None:
+    """
+    Converts and Quantizes the Transformer (results in an encoder and a decoder).
+    """
+    config = Config()
+    split_weights(config.filepaths.checkpoint)
+    path_to_encoder = convert_encoder()
+    simplify_onnx_model(path_to_encoder)
+    print(path_to_encoder)
+    path_to_encoder_fp16 = quantization_fp16(path_to_encoder, config.filepaths.encoder_path_fp16)
+    print(path_to_encoder_fp16)
+    path_to_decoder = convert_decoder()
+    simplify_onnx_model(path_to_decoder)
+    print(path_to_decoder)
+
+    # Only the decoder gets quantized.
+    # The segnet showed 80% worse performance on x86-64.
+    # Only improved size by around 15MB without any speedups
+    # (maybe even slowing inference down).
+    # FP16 slowed inference speed down (CPU).
+    path_to_decoder_fp16 = quantization_fp16(path_to_decoder, config.filepaths.decoder_path_fp16)
+    print(path_to_decoder_fp16)
+    path_to_decoder_int8 = quantization_int8(path_to_decoder, config.filepaths.decoder_path)
+    print(path_to_decoder_int8)
+    os.remove("decoder_weights.pt")
+    os.remove("encoder_weights.pt")
+
+
+if __name__ == "__main__":
+    # Converts pytorch models used by homr to onnx
+    convert_transformer_quant()
